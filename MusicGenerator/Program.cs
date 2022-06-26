d0cda3a0990c784e070777fa6f560d55ec05e8c6
D.Mentia <dimencia@gmail.com>
2022-06-26 18:40:17 -0400

    Move to a new project

---
+++ MusicGenerator/Program.cs
@@ -0,0 +1,50 @@
+﻿using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
+using Tensorflow;
+using NumSharp;
+
+// See https://aka.ms/new-console-template for more information
+
+
+// Parameters        
+var training_steps = 1000;
+var learning_rate = 0.01f;
+var display_step = 100;
+
+// Er, I guess this is declaring the data
+var X = np.array(3.3f, 4.4f, 5.5f, 6.71f, 6.93f, 4.168f, 9.779f, 6.182f, 7.59f, 2.167f,
+             7.042f, 10.791f, 5.313f, 7.997f, 5.654f, 9.27f, 3.1f);
+var Y = np.array(1.7f, 2.76f, 2.09f, 3.19f, 1.694f, 1.573f, 3.366f, 2.596f, 2.53f, 1.221f,
+             2.827f, 3.465f, 1.65f, 2.904f, 2.42f, 2.94f, 1.3f);
+
+var n_samples = X.shape[0]; // No clue what this is about, maybe the size of the input layer
+
+// We can set a fixed init value in order to demo
+var W = tf.Variable(-0.06f, name: "weight");
+var b = tf.Variable(-0.73f, name: "bias");
+var optimizer = keras.optimizers.SGD(learning_rate);
+
+// Run training for the given number of steps.
+foreach (var step in range(1, training_steps + 1))
+{
+    // Run the optimization to update W and b values.
+    // Wrap computation inside a GradientTape for automatic differentiation.
+    using var g = tf.GradientTape();
+    // Linear regression (Wx + b).
+    var pred = W * X + b;
+    // Mean square error.
+    var loss = tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * n_samples);
+    // should stop recording
+    // Compute gradients.
+    var gradients = g.gradient(loss, (W, b));
+
+    // Update W and b following gradients.
+    optimizer.apply_gradients(zip(gradients, (W, b)));
+
+    if (step % display_step == 0)
+    {
+        pred = W * X + b;
+        loss = tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * n_samples);
+        print($"step: {step}, loss: {loss.numpy()}, W: {W.numpy()}, b: {b.numpy()}");
+    }
+}
